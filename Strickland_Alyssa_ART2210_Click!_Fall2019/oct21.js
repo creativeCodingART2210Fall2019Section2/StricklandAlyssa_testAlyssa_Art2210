e0312405ed40c85369e8d2ede839472c1122bdd1
AlyssaStrickland <astri12@lsu.edu>
2019-10-21 12:33:34 -0500

    update

---
+++ Strickland_Alyssa_ART2210_Click!_Fall2019/oct21.js
@@ -0,0 +1,54 @@
+const radius = 50; //circle size will not change
+
+let a, b; //random locations for each click
+
+let score = 0;
+
+var count = 0; //time
+//var down = 15; //countdown
+
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+a = random(windowWidth);
+b = random(windowHeight);
+
+var timer = select('#timer');
+timer.html(count) //down - count to go backwards
+
+function timeIt(){
+  count++;
+  timer.html(count); //(down - count) to go backwards
+
+}
+setInterval(timeIt, 1000); //1000 miliseconds per second
+
+}
+
+  function draw() {
+background(125);
+fill(60, 0, 60);
+noStroke();
+ellipse(a, b, radius*2, radius*2)
+
+
+text("Score:" + score, 10, 600) //score points
+
+
+}
+
+function mousePressed(){ //click on the circle
+  let d = dist(mouseX, mouseY, a, b);
+  if (d< radius) {
+    a = random(windowWidth)
+    b = random(windowHeight)
+    score++;
+  }
+
+}
+
+
+function windowResize(){
+  resizeCanvas(windowWidth,windowHeight);
+
+}
