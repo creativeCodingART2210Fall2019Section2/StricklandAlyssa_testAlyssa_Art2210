f05943d3db94ad35198477458a869e78755951b2
AlyssaStrickland <55207004+AlyssaStrickland@users.noreply.github.com>
2019-12-02 13:49:06 -0600

    Update greekfamilytree.js

--- JSON_Exercise/greekfamilytree.js
+++ JSON_Exercise/greekfamilytree.js
@@ -18,7 +18,8 @@ function draw(){
   fill(255);
   text(gods.description, 500, 10);
   pop()
-  text(gods.greek_gods.chaos, 30, 30)
+  text(gods.greek_gods.chaos, 30, 30);
+  text(gods.greek_gods.erebus, 30, 80);
 
 }
 
