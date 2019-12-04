b1164535e6a615bbe597b7aad7df79881d94231c
AlyssaStrickland <astri12@lsu.edu>
2019-12-04 13:28:55 -0600

    update

--- JSON_Exercise/greekfamilytree.js
+++ JSON_Exercise/greekfamilytree.js
@@ -15,34 +15,34 @@ function setup(){
 function draw(){
 
   fill(255);
-  text(gods.description, 550, 10);
-  text(gods.greek_gods[17], 30, 60); //chaos
-   text(gods.greek_gods[19], 30, 60); //erebus
-   text(gods.greek_gods[26], 70, 60); //tartarus
-   text(gods.greek_gods[20], 90, 60); //Eros
-   text(gods.greek_gods[30], 110, 60); //Nyx
-   text(gods.greek_gods[22], 30, 90); //Uranus
-   text(gods.greek_gods[25], 50, 90); //Pontus
-   text(gods.greek_gods[29], 70, 90); //hemera
-   text(gods.greek_gods[31], 200, 70); //Nemesis
-   text(gods.greek_gods[28], 200, 100); //Thanatos
-   text(gods.greek_gods[21], 220, 100); //Hypnos
-   text(gods.greek_gods[18], 30, 110); //Chronos
-   text(gods.greek_gods[6], 60, 120); //Demeter
+  text(gods.description, 1100, 20);
+  text(gods.greek_gods[17], 60, 120); //chaos
+   text(gods.greek_gods[19], 60, 120); //erebus
+   text(gods.greek_gods[26], 140, 120); //tartarus
+   text(gods.greek_gods[20], 180, 120); //Eros
+   text(gods.greek_gods[30], 220, 120); //Nyx
+   text(gods.greek_gods[22], 60, 180); //Uranus
+   text(gods.greek_gods[25], 100, 180); //Pontus
+   text(gods.greek_gods[29], 140, 180); //hemera
+   text(gods.greek_gods[31], 400, 140); //Nemesis
+   text(gods.greek_gods[28], 400, 200); //Thanatos
+   text(gods.greek_gods[21], 440, 200); //Hypnos
+   text(gods.greek_gods[18], 60, 220); //Chronos
+   text(gods.greek_gods[6], 120, 240); //Demeter
    text(gods.greek_gods[12], 80, 120); //Hestia
-   text(gods.greek_gods[10], 100, 120); //Hera
-   text(gods.greek_gods[8], 100, 140); //Hades
-   text(gods.greek_gods[13], 120, 140); //Poseidon
-   text(gods.greek_gods[14], 140, 140); //Zeus
-   text(gods.greek_gods[2], 60, 160); //Apollo
-   text(gods.greek_gods[4], 80, 160); //Artemis
-   text(gods.greek_gods[5], 140, 160); //Athena
-   text(gods.greek_gods[9], 160, 160); //Hephaestus
-   text(gods.greek_gods[3], 180, 160); //Ares
-   text(gods.greek_gods[11], 120, 180); //Hermes
-   text(gods.greek_gods[1], 180, 180); //Aphrodite
-   text(gods.greek_gods[11], 100, 80); //Hermes
-   text(gods.greek_gods[7], 30, 200); //Hermes
+   text(gods.greek_gods[10], 200, 240); //Hera
+   text(gods.greek_gods[8], 200, 280); //Hades
+   text(gods.greek_gods[13], 240, 280); //Poseidon
+   text(gods.greek_gods[14], 280, 280); //Zeus
+   text(gods.greek_gods[2], 120, 320); //Apollo
+   text(gods.greek_gods[4], 160, 320); //Artemis
+   text(gods.greek_gods[5], 280, 320); //Athena
+   text(gods.greek_gods[9], 320, 320); //Hephaestus
+   text(gods.greek_gods[3], 360, 320); //Ares
+   text(gods.greek_gods[11], 120, 260); //Hermes
+   text(gods.greek_gods[1], 360, 360); //Aphrodite
+   text(gods.greek_gods[11], 200, 160); //Hermes
+   text(gods.greek_gods[7], 60, 400); //Hermes
 
 
 }
